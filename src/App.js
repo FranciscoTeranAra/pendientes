cf61e8c08738bf9aa482f0a5e6cee73215cefadd
P4NCH02798 <43188535+P4NCH02798@users.noreply.github.com>
2021-09-09 08:59:11 -0400

    Primer slice

---
+++ src/App.js
@@ -0,0 +1,8 @@
+const cantidad = document.querySelector("#Cantidad-input");
+
+form.addEventListener("submit", (event) => {
+    var porRecordatorio = document.forms["Recordatorio"].elementos[0].value;
+
+
+
+});
